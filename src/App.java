329b48b024778c9bea3093f66ec12d21d0340a6a
Daniel Wagner <daniel@MacBook-Pro-de-Daniel.local>
2021-06-02 09:17:43 -0300

    First commit '

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import desmoj.core.simulator.*;
+import java.util.concurrent.TimeUnit;
+
+public class App extends Model{
+	public App(Model arg0, String arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("aaa");
+	}
+
+	@Override
+	public String description() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doInitialSchedules() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+}
